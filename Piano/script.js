ac1045860a832f9956d23099ffc30ad967362136
Ian Geraldi Cordeiro <iangc49@gmail.com>
2022-11-26 16:36:23 -0300

    Gravações, playback e deleção

--- Piano/script.js
+++ Piano/script.js
@@ -3,7 +3,8 @@ const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 let tempo = 80
 let metronomeId = 0
 let currentRecording = ""
-let recordings = []
+let stopPlaying = false
+let recordings = [null,null,null,null,null]
 const metronomeAudio = new Audio('sounds/metronome.mp3')
 
 const keys = document.querySelectorAll('.key')
@@ -46,6 +47,11 @@ function tempoToMS(){
 
 
 function startRecording(){
+  if(recordings[0]!=null && recordings[1]!=null && recordings[2]!=null && recordings[3]!=null && recordings[4]!=null){
+    alert("All recording slots are full. Please delete one before continuing")
+    return
+  }
+  disableRecordingButtons()
   let tempoInput = document.getElementById("tempoInput").value;
   if(isNaN(tempoInput) || tempoInput < 30 || tempoInput > 240){
     alert("Please provide a valid tempo");
@@ -58,13 +64,70 @@ function startRecording(){
     tempoString = tempo.toString();
     currentRecording = currentRecording.concat(tempoString).concat("\n")
   }
-  console.log(currentRecording);
+  console.log(currentRecording)
 }
 function stopRecording(){
   clearInterval(metronomeId)
-  console.log(currentRecording)
-  recordings.push(currentRecording)
-  currentRecording = ""
+  for(let i = 0; i<=4; i++){
+    if(recordings[i]==null){
+      console.log("oi")
+      recordings[i] = currentRecording
+      currentRecording = ""
+      break
+    }
+  }
   document.getElementById("start").disabled = false
   document.getElementById("stop").disabled = true
+  enableRecordingButtons()
+}
+
+function enableRecordingButtons(){
+
+  for(let i = 1;i<=5;i++){
+    console.log(recordings[i-1])
+    if(recordings[i-1]!=null){
+      document.getElementById(`recording${i.toString()}`).disabled = false
+      document.getElementById(`deleteRecording${i.toString()}`).disabled = false
+    }
+  }
+}
+function disableRecordingButtons(){
+  for(let i = 1;i<=5;i++){
+    document.getElementById(`recording${i.toString()}`).disabled = true
+    document.getElementById(`deleteRecording${i.toString()}`).disabled = true
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function playRecording(recordingNum){
+  disableRecordingButtons()
+  document.getElementById("stopPlayback").disabled = false
+  recording = recordings[recordingNum-1]
+  notes = recording.split("\n")
+  bpm = notes[0]
+  for(let i = 1; i<notes.length; i++){
+    let audio = new Audio(`sounds/${notes[i]}.mp3`)
+    audio.play()
+    await sleep(tempoToMS(bpm))
+    if(stopPlaying){
+      enableRecordingButtons()
+      document.getElementById("stopPlayback").disabled = true
+      return
+    }
+  }
+  document.getElementById("stopPlayback").disabled = true
+  enableRecordingButtons()
+  return
+}
+function stopPlayback(){
+  stopPlaying = true
+}
+
+function deleteRecording(num){
+  recordings[num-1] = null
+  disableRecordingButtons()
+  enableRecordingButtons()
 }
