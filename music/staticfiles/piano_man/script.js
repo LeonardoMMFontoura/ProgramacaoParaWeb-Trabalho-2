272399fa5ba50e31ae8a6707f4db266587f28c55
Leo <leonardommfontoura@gmail.com>
2022-11-27 23:01:46 -0300

    staticfiles

---
+++ music/staticfiles/piano_man/script.js
@@ -0,0 +1,137 @@
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',']
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
+let tempo = 80
+let metronomeId = 0
+let currentRecording = null
+let stopPlaying = false
+let recordings = [null,null,null,null,null]
+const metronome = document.getElementById("metronome")
+
+const keys = document.querySelectorAll('.key')
+const whiteKeys = document.querySelectorAll('.key.white')
+const blackKeys = document.querySelectorAll('.key.black')
+
+keys.forEach(key => {
+  key.addEventListener('click', () => playNote(key))
+})
+
+
+document.addEventListener('keydown', e => {
+  if (e.repeat) return
+  const key = e.key
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key)
+  const blackKeyIndex = BLACK_KEYS.indexOf(key)
+
+  if (whiteKeyIndex > -1) playNote(whiteKeys[whiteKeyIndex])
+  if (blackKeyIndex > -1) playNote(blackKeys[blackKeyIndex])
+})
+
+function playNote(key) {
+  const noteAudio = document.getElementById(key.dataset.note)
+  if(currentRecording!=null){
+    currentRecording = currentRecording.concat(noteAudio.id.toString()).concat("\n")
+  }
+  noteAudio.currentTime = 0.2
+  noteAudio.play()
+  key.classList.add('active')
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active')
+  })
+}
+
+function playMetronome(){
+  metronome.currentTime = 0
+  metronome.play()
+}
+function tempoToMS(){
+  return 60000/tempo
+}
+
+
+function startRecording(){
+  if(recordings[0]!=null && recordings[1]!=null && recordings[2]!=null && recordings[3]!=null && recordings[4]!=null){
+    alert("All recording slots are full. Please delete one before continuing")
+    return
+  }
+  let tempoInput = document.getElementById("tempoInput").value;
+  if(isNaN(tempoInput) || tempoInput < 30 || tempoInput > 240){
+    alert("Please provide a valid tempo");
+  }
+  else{
+    currentRecording=""
+    disableRecordingButtons()
+    document.getElementById("start").disabled = true
+    document.getElementById("stop").disabled = false
+    tempo = tempoInput;
+    metronomeId = setInterval(playMetronome, tempoToMS())
+    tempoString = tempo.toString();
+    currentRecording = currentRecording.concat(tempoString).concat("\n")
+  }
+  console.log(currentRecording)
+}
+function stopRecording(){
+  clearInterval(metronomeId)
+  for(let i = 0; i<=4; i++){
+    if(recordings[i]==null){
+      console.log(currentRecording)
+      recordings[i] = currentRecording
+      currentRecording = null
+      break
+    }
+  }
+  document.getElementById("start").disabled = false
+  document.getElementById("stop").disabled = true
+  enableRecordingButtons()
+}
+
+function enableRecordingButtons(){
+
+  for(let i = 1;i<=5;i++){
+    if(recordings[i-1]!=null){
+      document.getElementById(`recording${i.toString()}`).disabled = false
+      document.getElementById(`deleteRecording${i.toString()}`).disabled = false
+    }
+  }
+}
+function disableRecordingButtons(){
+  for(let i = 1;i<=5;i++){
+    document.getElementById(`recording${i.toString()}`).disabled = true
+    document.getElementById(`deleteRecording${i.toString()}`).disabled = true
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function playRecording(recordingNum){
+  disableRecordingButtons()
+  document.getElementById("stopPlayback").disabled = false
+  recording = recordings[recordingNum-1]
+  console.log(recording)
+  notes = recording.split("\n")
+  bpm = notes[0]
+  for(let i = 1; i<notes.length-1; i++){
+    let noteAudio = document.getElementById(notes[i])
+    noteAudio.play()
+    await sleep(tempoToMS(bpm))
+    if(stopPlaying){
+      enableRecordingButtons()
+      document.getElementById("stopPlayback").disabled = true
+      stopPlaying = false
+      return
+    }
+  }
+  document.getElementById("stopPlayback").disabled = true
+  enableRecordingButtons()
+  return
+}
+function stopPlayback(){
+  stopPlaying = true
+}
+
+function deleteRecording(num){
+  recordings[num-1] = null
+  disableRecordingButtons()
+  enableRecordingButtons()
+}
